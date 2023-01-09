c85ca46ba95f13defad86fc26b634677249c8de5
Aditya Deshpande <adidesx@gmail.com>
2023-01-09 18:34:38 +0530

    Sandboxing Tool Development

---
+++ Sandboxing/fileStructures.hpp
@@ -0,0 +1,8 @@
+
+typedef struct 
+{
+	const char *pathName;
+	const uid_t user;
+	const gid_t group;
+	const mode_t perm;
+} aCreateDirs;
---
+++ Sandboxing/userGroup.hpp
@@ -0,0 +1,12 @@
+#ifndef USER_GROUP_IDS_HPP
+#define USER_GROUP_IDS_HPP
+
+#define USER_ROOT 0
+#define	USER_LP	7
+#define	USER_MAIL 8
+
+#define	GROUP_ROOT 0
+#define	GROUP_LP 7
+#define	GROUP_MAIL 8
+
+#endif
