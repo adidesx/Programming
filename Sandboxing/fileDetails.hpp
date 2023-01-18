d94788495020311fa8355adf8b0a37e9956192e6
Aditya Deshpande <adidesx@gmail.com>
2023-01-18 20:46:10 +0530

    Deleted unwanted files

--- Sandboxing/fileDetails.hpp
+++
@@ -1,8 +0,0 @@
-
-const aCreateDirs iCreateDirs[] =
-{
-	{ "/home/adi/Programming/Sandboxing/Aditya/One",	USER_ROOT,	GROUP_ROOT,	S_IRUSR|S_IWUSR|S_IRGRP|S_IRGRP	},
-	{ "/home/adi/Programming/Sandboxing/Aditya/Two",	USER_LP,	GROUP_LP,	S_IRUSR|S_IWUSR 		},
-	{ "/home/adi/Programming/Sandboxing/Aditya/Three",	USER_MAIL,	GROUP_MAIL,	S_IRGRP|S_IWGRP			},
-	{ NULL,		0,		0,		0				}
-};
