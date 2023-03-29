afe3306590f61d71c1192b76a309d771669d8504
Aditya Deshpande <adidesx@gmail.com>
2023-03-29 12:40:14 +0530

    Deleted unwanted files

--- Sandboxing/include/createDir.hpp
+++
@@ -1,6 +0,0 @@
-#include "fileStructures.hpp"
-#include "userGroup.hpp"
-#include "fileDetails.hpp"
-
-void createAllDirs( const aCreateDirs* dCreateDirs );
-bool createDirs( const char *pathName, const uid_t user, const gid_t group, const mode_t perm );
--- Sandboxing/include/fileStructures.hpp
+++
@@ -1,8 +0,0 @@
-
-typedef struct 
-{
-	const char *pathName;
-	const uid_t user;
-	const gid_t group;
-	const mode_t perm;
-} aCreateDirs;
